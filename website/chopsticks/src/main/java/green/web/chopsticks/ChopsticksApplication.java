65a8bbcbeab33967b17ca9656feff35faac228b5
NinjaBob <153332837+NinjaBob123@users.noreply.github.com>
2025-08-03 18:36:33 -0500

    Create initial project

---
+++ website/chopsticks/src/main/java/green/web/chopsticks/ChopsticksApplication.java
@@ -0,0 +1,13 @@
+package green.web.chopsticks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChopsticksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChopsticksApplication.class, args);
+	}
+
+}
---
+++ website/chopsticks/src/test/java/green/web/chopsticks/ChopsticksApplicationTests.java
@@ -0,0 +1,13 @@
+package green.web.chopsticks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChopsticksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
